7a0acf3d895cfee99a6c7b6934786cbc731d42d6
Vishwang Dave <vishwangdave@gmail.com>
2021-02-03 00:08:20 +0530

    added: -base and basic structure of site -husekeeping files necessary for django side

---
+++ face_similarity/Janus/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path,include
+from . import views
+
+
+urlpatterns = [
+    path('',views.IndexView.as_view(),name='home'),
+    path('score/',views.Janus_API.as_view(),name='janus_api')
+]
